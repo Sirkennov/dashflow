a7217f6d9f84081c2db0fa5ed4aefd4952947731
Diex Díaz <victordiex@gmail.com>
2025-07-10 00:43:30 -0400

    feat: Instala tailwindcss y lo prepara para produccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';  // Importa el plugin de Tailwind
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(), // Añade el plugin de Tailwind aquí
+  ],
+});
