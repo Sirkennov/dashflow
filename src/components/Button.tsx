e853338e8f1c404b04af36cb7a7bb56d276fa130
Diex Díaz <victordiex@gmail.com>
2025-07-12 00:52:41 -0400

    fix: Corrige los estilos para poder cambiar los colores del botón

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,9 +9,8 @@ export const Button: React.FC<ButtonProps> = ({ children, className, ...rest }) 
     return (
         <button
             className={`
-        bg-red-500 hover:bg-red-700 text-white 
-        font-bold py-2 px-4 rounded focus:outline-none 
-        focus:shadow-outline cursor-pointer ${className}`}
+                text-white font-bold py-2 px-4 rounded focus:outline-none 
+                focus:shadow-outline cursor-pointer ${className}`}
             {...rest} // Aquí se pasan todas las demás propiedades HTML (type, onClick, disabled, etc.)
         >
             {children}
